0dfacd151a2b4c45b80d704e104f33a40dba1ba9
Vladyslav Bondarenko <vlad.bond05@gmail.com>
2020-05-11 21:25:13 +0300

    added projects state

---
+++ src/store/modules/projects.js
@@ -0,0 +1,32 @@
+// import api from '../../services/api';
+
+import api from '../../services/api';
+
+export default {
+  state: {
+    projects: [],
+  },
+  getters: {
+    projectCount: (state) => state.projects.length,
+  },
+  mutations: {
+    setProjects(state, projects) {
+      state.projects = projects;
+    },
+  },
+  actions: {
+    async fetchProjects({ commit }) {
+      commit('setLoading', true);
+      try {
+        const { data } = await api.get('projects/private');
+        commit('setProjects', data);
+        commit('clearError');
+      } catch (error) {
+        commit('setError', error.statusCode);
+        throw error;
+      } finally {
+        commit('setLoading', false);
+      }
+    },
+  },
+};
