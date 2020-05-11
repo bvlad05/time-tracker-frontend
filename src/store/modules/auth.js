d8ba6d215fef220503d786d28c59a804de063850
Vladyslav Bondarenko <vlad.bond05@gmail.com>
2020-05-11 21:25:36 +0300

    Update auth.js

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -4,22 +4,12 @@ import api from '../../services/api';
 export default {
   state: {
     token: cookies.get('token') || '',
-    user: cookies.get('user') ? JSON.parse(cookies.get('user')) : null,
   },
   getters: {
     isAuth: (state) => !!state.token,
-    user: (state) => state.user,
     token: (state) => state.token,
   },
   mutations: {
-    setUser(state, user) {
-      state.user = user;
-      cookies.set('user', user);
-    },
-    clearUser(state) {
-      state.user = null;
-      cookies.remove('user');
-    },
     setToken(state, token) {
       state.token = token;
       cookies.set('token', token);
@@ -37,7 +27,6 @@ export default {
         const { data } = await api.post('auth/local', JSON.stringify({ identifier, password }));
         const { jwt: token } = data;
         commit('setToken', token);
-        commit('setUser', data.user);
         commit('clearError');
       } catch (error) {
         commit('setError', error.response.data.message[0].messages[0].id);
@@ -52,7 +41,6 @@ export default {
         const { data } = await api.post('auth/local/register', JSON.stringify({ ...user }));
         const { jwt: token } = data;
         commit('setToken', token);
-        commit('setUser', data.user);
         commit('clearError');
       } catch (error) {
         commit('setError', error.response.data.message[0].messages[0].id);
@@ -62,7 +50,6 @@ export default {
       }
     },
     logoutUser({ commit }) {
-      commit('clearUser');
       commit('clearToken');
     },
   },
